44c2beaa91b95d6b84563de58e391f9ea7f6472b
Damien Nelson <dhnisaman@gmail.com>
2023-07-26 19:52:52 -0400

    commit tree f61bd5ab7f525d3022ce85ce75311d972dde10e7

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,10 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch(action.type){
+    case 'friends/add':
+      return {...state, friends: [...state.friends, action.payload]}
+    case 'friends/remove': 
+      return {...state, friends: state.friends.filter(friend => action.payload.name === friend.name)}
+    default:
+      return state
+  }
 }
